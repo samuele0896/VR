6af4611347e0223edea254c0d6e81c3c7489f309
samuele0896 <samu.barto96@gmail.com>
2025-01-08 18:37:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("chooseLink").addEventListener("click", function () {
+  const linkOptions = document.getElementById("linkOptions");
+  linkOptions.style.display = "block";
+});
